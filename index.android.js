5b75830dc3c413d0f9086f1db634bd9c784d4610
Cristi Ielciu <cristielciu@yahoo.com>
2016-10-13 22:33:37 +0300

    Using props and state tutorial

--- index.android.js
+++ index.android.js
@@ -1,12 +1,43 @@
 AppRegistry.registerComponent('MyProject', () => MyProject);
 
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, Text, View } from 'react-native';
+
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {showText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText });
+    }, 1000);
+  }
+
+  render() {
+    let display = this.state.showText ? this.props.text : ' ';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
+
+class Greeting extends Component {
+  render() {
+    return (
+      <Blink text={'Hello ' + this.props.name} />
+    );
+  }
+}
 
 class MyProject extends Component {
   render() {
     return (
-      <Text>Hello world!</Text>
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Ielciu ' />
+        <Greeting name='Cristian' />
+        <Greeting name='Vlad' />
+      </View>
     );
   }
 }
