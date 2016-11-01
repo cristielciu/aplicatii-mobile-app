26d53f3bad64f6f1d62895b578234b8a43de43ab
Cristi Ielciu <cristielciu@yahoo.com>
2016-11-01 21:33:23 +0200

    Redesigned index page to login page

--- index.android.js
+++ index.android.js
@@ -1,7 +1,23 @@
 AppRegistry.registerComponent('MyProject', () => MyProject);
 
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, TextInput, View, ScrollView, Switch, TouchableNativeFeedback } from 'react-native';
+
+class Input extends Component {
+  render() {
+    return (
+      <View >
+        <Text>
+          {this.props.fieldName}
+        </Text>
+        <TextInput
+          placeholder="Type here..."
+          onChangeText={(text) => this.setState({text})}
+        />
+      </View>
+    );
+  }
+}
 
 class Blink extends Component {
   constructor(props) {
@@ -31,6 +47,13 @@ class Greeting extends Component {
 }
 
 class MyProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+  state = {
+    SwitchIsOn: false,
+  };
   render() {
     return (
       <View style={{flex: 1}}>
@@ -39,14 +62,56 @@ class MyProject extends Component {
           <Greeting name='Cristian'/>
           <Greeting name='Vlad' />
         </View>
-        <View style={{flex: 1, backgroundColor: 'steelblue', flexDirection: 'row', justifyContent: 'space-between'}}>
-          <View style={{flex: 1, height: 50 , backgroundColor: 'powderblue'}} />
-          <View style={{flex: 1, height: 50, backgroundColor: 'skyblue', alignItems: 'center', justifyContent: 'center',}} >
-            <Text style={{color: '#EBF5FB'}}>
-              Meniu
-            </Text>
+        <View style={{flex: 1, backgroundColor: 'steelblue', flexDirection: 'column', justifyContent: 'space-between'}}>
+          <View style={{flex: 1, backgroundColor: 'steelblue', flexDirection: 'row', justifyContent: 'space-between'}}>
+            <View style={{flex: 1, height: 50 , backgroundColor: 'powderblue'}} />
+            <View style={{flex: 1, height: 50, backgroundColor: 'skyblue', alignItems: 'center', justifyContent: 'center',}} >
+              <Text style={{color: '#EBF5FB'}}>
+                Login
+              </Text>
+            </View>
+            <View style={{flex: 1, height: 50 ,backgroundColor: 'powderblue'}} />
           </View>
-          <View style={{flex: 1, height: 50 ,backgroundColor: 'powderblue'}} />
+          <View style={{flex: 6}}>
+            <Input fieldName='Username: ' />
+            <View>
+              <Text>
+                Password:
+              </Text>
+              <TextInput
+                placeholder="Type here password"
+                onChangeText={(text) => this.setState({text})}
+              />
+            </View>
+            <ScrollView>
+              <Text style={{fontSize:16}}>Some terms: </Text>
+              <Text style={{fontSize:16}}>Cristi Ielciu''s Terms: </Text>
+              <Text style={{fontSize:16}}>11111111111111111111111: </Text>
+              <Text style={{fontSize:16}}>22222: </Text>
+              <Text style={{fontSize:16}}>333333333 </Text>
+              <Text style={{fontSize:16}}>44444444444444 </Text>
+              <Text style={{fontSize:16}}>55555555555555555 </Text>
+              <Text style={{fontSize:16}}>66666666666666666666 </Text>
+              <Text style={{fontSize:16}}>Cristi Ielciu''s Terms8: </Text>
+              <Text style={{fontSize:16}}>Cristi Ielciu''s Terms9: </Text>
+            </ScrollView>
+            <View style={{backgroundColor: 'steelblue', flexDirection: 'row', justifyContent: 'space-between'}}>
+                <Text style={{fontSize:20}}>
+                  Login only if you accept the terms
+                </Text>
+                <Switch
+                  onValueChange={(value) => this.setState({SwitchIsOn: value})}
+                  style={{marginBottom: 10}}
+                  value={this.state.SwitchIsOn} />
+            </View>
+            <TouchableNativeFeedback
+                onPress={this._onPressButton}
+                background={TouchableNativeFeedback.SelectableBackground()}>
+              <View style={{backgroundColor: 'powderblue'}}>
+                <Text style={{textAlign: 'center', fontSize: 25}}>Login</Text>
+              </View>
+            </TouchableNativeFeedback>
+          </View>
         </View>
       </View>
     );
